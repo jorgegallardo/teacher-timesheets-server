f5fe7896d4f4b53928cc4a840b7d5b278bddccae
Jorge Gallardo <jorge8486@gmail.com>
2020-12-21 20:22:01 -0500

    added body parser

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
 
 app.get("", (req, res) => {
   res.send("it works");
+});
+
+app.post("/pdf", (req, res) => {
+  console.log(req.body);
+  res.status(201).json({ message: "pdf created" });
 });
 
 app.listen(3000, () => {
