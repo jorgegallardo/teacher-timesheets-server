f3adc8b9ad0c370197a52fdf17ea100b9638c095
Jorge Gallardo <jorge8486@gmail.com>
2020-12-27 12:35:04 -0500

    added cors, can receive timesheet

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.get("", (req, res) => {
-  res.send("it works");
+  res.send('it works');
 });
 
-app.post("/pdf", (req, res) => {
+app.post("/create", (req, res) => {
   console.log(req.body);
-  res.status(201).json({ message: "pdf created" });
+  res.status(201).json({ message: 'timesheet created successfully' });
 });
 
 app.listen(3000, () => {
-  console.log("server is running");
+  console.log('server is running');
 });
